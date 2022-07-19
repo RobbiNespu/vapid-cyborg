2d12b68f2027a1fc5b3baf8fd42856e15596607e
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-07-19 14:42:08 -0500

    created micro post type for right sidebar

--- footer.php
+++ footer.php
@@ -1,10 +1,27 @@
-        <div id="right-sidebar">
-            <h2>Right Sidebar</h2>
-        </div>
-    </div>        
-        <div id="copyright">
-           <p>all nonderivative content is copyright under the MIT license. some rights reserverd.</p>
-        </div>
+
+<div id="right-sidebar">
+
+    <?php
+        $args =  array(
+        'post_type' => 'micro-post',
+        'orderby' => 'menu_order',
+        'order' => 'ASC'
+        );
+        $custom_query = new WP_Query( $args );
+                    while ($custom_query->have_posts()) : $custom_query->the_post();
+    ?>
+    
+    <div class="micro-post">
+    <h2 class="micro-post-title"><?php the_title(); ?></h2>
+    <?php the_excerpt(); ?>
+    </div>
+    <?php endwhile; ?>
+
+    </div>
+</div>        
+<div id="copyright">
+    <p>all nonderivative content is copyright under the MIT license. some rights reserverd.</p>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -29,21 +29,49 @@ function custom_settings_page() { ?>
 	</div>
 <?php }
 
-// Twitter
+// profile
+function setting_profile() { ?>
+	<input type="text" name="profile" id="profile" value="<?php echo get_option( 'profile' ); ?>" />
+<?php }
+
+// twitter
 function setting_twitter() { ?>
 	<input type="text" name="twitter" id="twitter" value="<?php echo get_option( 'twitter' ); ?>" />
 <?php }
 
+// github
 function setting_github() { ?>
 	<input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
 <?php }
 
 function custom_settings_page_setup() {
 	add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+    add_settings_field( 'profile', 'Profile Picture URL', 'setting_profile', 'theme-options', 'section' );
     add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
     add_settings_field( 'github', 'GitHub URL', 'setting_github', 'theme-options', 'section' );
     
+    register_setting( 'section', 'profile' );
     register_setting( 'section', 'twitter' );
     register_setting( 'section', 'github' );
 }
 add_action( 'admin_init', 'custom_settings_page_setup' );
+
+// micro news post type
+function create_micro_post() {
+	register_post_type( 'micro-post',
+			array(
+			'labels' => array(
+	'name' => __( 'Micro Post' ),
+	'singular_name' => __( 'Micro Post' ),
+			),
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array(
+	'title',
+	'editor',
+	'thumbnail',
+	'custom-fields'
+			)
+	));
+}
+add_action( 'init', 'create_micro_post' );
