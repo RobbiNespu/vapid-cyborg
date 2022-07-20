08dc6e1188bf88bcd8b5f91a433226e8fca7e643
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-07-19 20:49:34 -0500

    read more link is h5 so it doesn't get styled as a warning

--- functions.php
+++ functions.php
@@ -78,6 +78,6 @@ add_action( 'init', 'create_micro_post' );
 
 // better read more link
 function modify_read_more_link() {
-    return '<h6><a class="more-link" href="' . get_permalink() . '">read more</a></h6>';
+    return '<h5><a class="more-link" href="' . get_permalink() . '">read more</a></h5>';
 }
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
