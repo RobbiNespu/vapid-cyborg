592d4612b39667c182f0db790ca82376a5d13ac3
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-07-19 20:29:43 -0500

    better read more link

--- functions.php
+++ functions.php
@@ -75,3 +75,9 @@ function create_micro_post() {
 	));
 }
 add_action( 'init', 'create_micro_post' );
+
+// better read more link
+function modify_read_more_link() {
+    return '<p><a class="more-link" href="' . get_permalink() . '">read more</a></p>';
+}
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
