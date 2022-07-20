4075c8e2eaaec579c9e08cd6201ca7e92dc9a643
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-07-19 23:20:53 -0500

    added custom 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+        <div id="content">
+            <div>
+                <h1>Error 404</h1>
+                <h5>Well, this is embarassing...</h5>
+            </div>
+            <div class="pt-4">
+                <p>There doesn't seem to be anything here. Maybe I forgot to make whatever was supposed to be here, or maybe it dissapeared somehow. Either way, this isn't the page you're looking for.</p>
+            </div>
+        </div>
+<?php get_footer(); ?>
