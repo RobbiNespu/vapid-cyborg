9e42d86225397b866b3a83ff48af077503910941
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-08-05 12:54:30 -0500

    fixed buttons not being fully links

--- sidebar.php
+++ sidebar.php
@@ -5,10 +5,10 @@
                 </div>
                 <div id="author-block-r" class="flex flex-col place-content-center">
                     <div class="meta text-center">contact me via:</div>
-                    <button type="button" class="slate"><a href="<?php echo get_option('twitter'); ?>">Mastodon</a></button>
-                    <button type="button" class="slate"><a href="<?php echo get_option('matrix'); ?>">Matrix</a></button>
-                    <button type="button" class="slate"><a href="<?php echo get_option('email'); ?>">Email</a></button>
-                    <button type="button" class="slate"><a href="<?php echo get_option('github'); ?>">GitHub</a></button>
+                    <a href="<?php echo get_option('twitter'); ?>"><button type="button" class="slate">Mastodon</button></a>
+                    <a href="<?php echo get_option('matrix'); ?>"><button type="button" class="slate">Matrix</button></a>
+                    <a href="<?php echo get_option('email'); ?>"><button type="button" class="slate">Email</button></a>
+                    <a href="<?php echo get_option('github'); ?>"><button type="button" class="slate">GitHub</button></a>
                 </div>
             </div>
             <div id="title" class="my-4 text-center lg:text-left">
