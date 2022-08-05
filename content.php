f5df1d4b65d5badb3183260a9a3b49f9ef471682
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-08-05 01:18:55 -0500

    added more padding between blog posts

--- content.php
+++ content.php
@@ -1,4 +1,4 @@
-<div class="blog-post">
+<div class="blog-post mb-8">
 	<h2 class="text-xl"><?php the_title(); ?></h2>
 	<div class="meta">posted <?php the_date(); ?> by <a href="#"><?php the_author(); ?></a> | <a href="<?php the_permalink(); ?>">permalink</a></div>
     <div class="p-4">
