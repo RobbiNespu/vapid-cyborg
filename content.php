950e210e4fc83800fd29c79b25a70c4dbdf4a9cd
aphotic <109552208+4ph071c@users.noreply.github.com>
2022-11-30 10:18:58 -0600

    made author links actually work

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <div class="blog-post mb-8">
 	<h2 class="text-2xl"><?php the_title(); ?></h2>
-	<div class="meta">posted <?php the_date(); ?> by <a href="#"><?php the_author(); ?></a> | <a href="<?php the_permalink(); ?>">permalink</a></div>
+	<div class="meta">posted <?php the_date(); ?> by <?php the_author_link(); ?> | <a href="<?php the_permalink(); ?>">permalink</a></div>
     <div class="p-4">
         <?php the_content(); ?>
     </div>
