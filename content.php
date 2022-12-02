421b4a04daca7a5f24ff128d082c6d1d1da6e84c
aphotic <aphotic@corrupted.space>
2022-12-02 09:42:43 -0600

    fixed h1 and h2 to make them bigger

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <div class="blog-post mb-8">
-    <h2 class="text-2xl"><?php the_title(); ?></h2>
+    <h2><?php the_title(); ?></h2>
     <div class="meta">posted <?php the_date(); ?> by <?php the_author_link(); ?> | <a href="<?php the_permalink(); ?>">permalink</a></div>
     <div class="m-4 p-2 rounded border-stone-600 border bg-gradient-to-b from-stone-800">
         <?php the_content(); ?>
